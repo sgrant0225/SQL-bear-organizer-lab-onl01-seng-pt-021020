05f59183cd94dc97aae875c4fbc453abcf48b1af
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-11 19:16:42 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'female', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, 'male', 'white', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 10, 'male', 'black', 'fiesty', 1 );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'male', 'light-brown', 'quiet', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, 'female', 'light-brown', 'serious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 8, 'male', 'red', 'mean', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 4, 'female', 'black', 'friendly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, 'male', 'brown', 'serious', 0);
