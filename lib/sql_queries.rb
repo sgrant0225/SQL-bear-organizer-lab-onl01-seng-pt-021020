f004ace9be0ee760c0649272a01cfc41c5b23f66
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-11 20:44:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperaments, COUNT(color) FROM bears WHERE color = 'dark brown'";
+  "SELECT temperaments, COUNT() FROM bears WHERE color = 'dark brown'";
 end
 
 def selects_bear_that_killed_Tim
